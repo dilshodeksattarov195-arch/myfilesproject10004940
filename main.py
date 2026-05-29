7894cef68cbaa082ef48c794cd05d9503f0f8997
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-29 19:13:01 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class cartDaveWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = cartDaveWorker()
+    print("Worker engine initialized.")
